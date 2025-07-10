6bbeb93644765687f66218cac75b2d8cb56b9bce
HelloFiona6 <1870341385@qq.com>
2025-07-10 14:09:15 -0400

    init

---
+++ backend/db.js
@@ -0,0 +1,36 @@
+// backend/db.js
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./survey.db');
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    "group" TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`);
+    db.run(`CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL,
+    content TEXT NOT NULL,
+    answer TEXT NOT NULL
+    )`);
+    db.run(`CREATE TABLE IF NOT EXISTS practice_tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type TEXT NOT NULL,
+    content TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    strategy TEXT
+    )`);
+    db.run(`CREATE TABLE IF NOT EXISTS responses (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INT,
+    task_id INT,
+    is_practice BOOLEAN,
+    response TEXT,
+    correct BOOLEAN,
+    time_spent INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`);
+});
+
+module.exports = db;
